17eeb40480fe7d093f8e8cc88dc212f517677a7c
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-04-04 22:27:00 +0200

    dz

---
+++ script/main.js
@@ -0,0 +1,49 @@
+// --1--
+
+// let name = "Джон";
+// let admin = name;
+// alert(admin);
+
+// -----
+
+// --2--??
+
+// let planet = "Earth";
+// let user = "some user";
+// console.info(planet, user);
+
+// -----
+
+// --3--
+
+// let warn1 = prompt('1');
+// let warn2 = prompt('2');
+// let warn3 = prompt('3');
+// console.warn(warn1);
+// console.warn(warn2);
+// console.warn(warn3);
+
+// -----
+
+// --4--
+
+// let year = prompt('Напишіть ваш рік народження');
+// let name = prompt('Напишіть як вас звати');
+// if(2023 - year >= 18){
+//     console.log(name + ". Ви можете йти гуляти.");
+// }else if(2023 - year < 18){
+//     console.log(name + ". Вам заборонено йти гуляти.");
+// }
+
+// -----
+
+// --5--
+
+    // let number = prompt('Введіть число від 10 до 1100');
+    // if( 10 <= number && number <= 1100){
+    //     console.log(number, '. Входить до діапазону чисел від 10 до 1100');
+    // }else{
+    //     console.log(number, '. Не входить до діапазону чисел від 10 до 1100');
+    // }
+
+// -----

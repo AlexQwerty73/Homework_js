cb25eb858d17e0a698bb4426aa39f404256ae5f5
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-04-07 12:31:30 +0200

    update + fix

--- script/main.js
+++ script/main.js
@@ -25,42 +25,31 @@ October, November, December
 const month = 'February';
 
 switch (month) {
+
     case 'January':
-        console.log("Winter");
-        break;
     case 'February':
-        console.log("Winter");
-        break;
     case 'December':
         console.log("Winter");
         break;
+
     case 'May':
-        console.log("Spring");
-        break;
     case 'April':
-        console.log("Spring");
-        break;
     case 'March':
         console.log("Spring");
         break;
+
     case 'June':
-        console.log("Summer");
-        break;
     case 'July':
-        console.log("Summer");
-        break;
     case 'August':
         console.log("Summer");
         break;
+
     case 'September':
-        console.log("Autumn");
-        break;
     case 'October':
-        console.log("Autumn");
-        break;
     case 'November':
         console.log("Autumn");
         break;
+
     default:
         console.log('??');
         break;
@@ -68,26 +57,35 @@ switch (month) {
 // 4
 
 number = 5;
-power = 2;
-let pow = (num, pow) => {
+const power = 2;
+const pow = (num, pow) => {
     return Math.pow(num, pow);
 }
 console.log(pow(number, power));
-
 // 5
+
+const string5 = "Doby";
+number = 5;
+
+const StringRepeat = (num, str) => {
+    return num != 1? str + StringRepeat(num - 1, str): str;
+}
+console.log(StringRepeat(number, string5));
+
+// 6
 const maxNumber = 10;
-let random_5 = (maxNum) => {
+let random_6 = (maxNum) => {
     return Math.random() * maxNum;
 }
-console.log(random_5(maxNumber));
-// 6
+console.log(random_6(maxNumber));
+// 7
 let numArray = [3, 4, 73, 24, 8, 2, 80];
 
 const getMaxNumber = (arr) => {
     return Math.max.apply(null, arr);
 }
 console.log(getMaxNumber(numArray));
-// 7
+// 8
 const password_ = 'n8jgf7i';
 const password__ = 'iytibyi';
 
@@ -95,16 +93,35 @@ const password_check = (pas, pas_) => {
     return (pas == pas_ ? true : false);
 }
 console.log(password_check(password_, password__));
-// 8    НЕ ПРАЦЮE
-let number8_1 = 5;
-let number8_2 = 25;
-const abs_func = (n1, n2) => {
-   return Math.abs(n1 + n2);
+// 9   
+const number9_1 = 5;
+const number9_2 = 25;
+const absFunc = (n1, n2) => {
+    return Math.abs(n1 + n2);
 }
-console.log(abs_func(number8_1, number8_2));
-// 9
+console.log(absFunc(number9_1, number9_2));
+// 10
 
-let random_9 = () => {
+let random_10 = () => {
     return Math.random() * 10;
 }
-console.log(random_9());
+console.log(random_10());
+
+// 11
+
+number = 3.8;
+
+const getFactorial = (num) => {
+    return (num != 1 ? num * getFactorial(Math.floor(num) - 1) : 1);
+}
+console.log(getFactorial(number));
+
+// 12
+
+number = 89;
+
+const sqrtFunc = (num) => {
+    return 
+}
+console.log(sqrtFunc(number));
+// 12-?
